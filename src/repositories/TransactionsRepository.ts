e1e610324bcc0852ac8d3fd7aadf58a64bd860ce
valber <valbermarcelino99@gmail.com>
2021-01-05 02:26:51 -0300

    completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO{
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,57 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
+  
   public getBalance(): Balance {
-    // TODO
+    /*var lista = [];
+    lista.push(valor);*/
+
+    /*function agruparPor(objetoArray: Transaction[], propriedade: 'income' | 'outcome') {
+      return objetoArray.reduce(function (acc, obj) {
+        let key = obj[propriedade]; //obj.type
+        if (!acc[key]) {
+          acc[key] = [];
+        }
+        acc[key].push(obj);
+        return acc;
+      }, {});
+    }*/
+
+    //const balance = this.transactions.reduce(
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+          default:
+            break;
+        }
+
+        return accumulator;
+      }, { //valor inicial/já faz o js ententer formato do accumulator
+        income: 0,
+        outcome: 0,
+        total: 0,
+      }
+    )
+
+    var total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
